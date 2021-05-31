4b7e90343b2c373a1a5010a7e6839c93d86b7525
ilwel <ilwelisaac@hotmail.com>
2021-05-31 18:33:56 -0300

    input implementation

--- PM.py
+++ PM.py
@@ -1,7 +1,25 @@
 import simplex
-obj_func = "10x + y" # ---DEFINA A FUNÇÃO OBJETIVO AQUI---
-obj = 'MAX' # APENAS MAXIMIZAÇÃO IMPLEMENTADA
-restricoes = ['x + y <= 200', '2x + 2y <= 600'] # ---DEFINA AS RESTRIÇÕES AQUI---
+# obj_func = "10x + y" # ---DEFINA A FUNÇÃO OBJETIVO AQUI---
+# obj = 'MAX' # APENAS MAXIMIZAÇÃO IMPLEMENTADA
+# restricoes = ['x + y <= 200', '2x + 2y <= 600'] # ---DEFINA AS RESTRIÇÕES AQUI---
+
+#recebendo variáveis por input:: iuezin da massa esteve aqui
+obj_func = input('defina a função do objeto: ')
+obj = input('defina o MAX: ')
+restricoes = []
+while(True):
+
+    prox = input('deseja colocar mais uma restrição?:(y/n) ')
+    if(prox == 'y'):
+        restricao  = input('defina uma restrição: ')
+        restricoes.append(restricao)
+    elif(prox == 'n'):
+        break
+    else:
+        print('coloque uma opção válida')
+    
+print(restricoes)
+
 simplex = simplex.Simplex(obj_func, obj)
 if len(restricoes) > 0:
     for k in restricoes:
